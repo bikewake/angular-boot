bbe27749cb597edb284a920896260ba2a571b700
Lubo Sladok <bikewake@gmail.com>
2023-11-21 19:46:43 +0100

    add angular 17 keycloak oidc auth

---
+++ chatapp/src/app/app.config.ts
@@ -0,0 +1,44 @@
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { AuthConfig, OAuthService, provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideHttpClient } from '@angular/common/http';
+
+export const authCodeFlowConfig: AuthConfig = {
+  issuer: 'https://buben.bikewake.org/realms/chat',
+  tokenEndpoint: 'https://buben.bikewake.org/realms/chat/protocol/openid-connect/token',
+  redirectUri: window.location.origin,
+  clientId: 'angular-local',
+  responseType: 'code',
+  scope: 'openid profile',
+  showDebugInformation: true,
+};
+
+function initializeOAuth(oauthService: OAuthService): Promise<void> {
+  return new Promise((resolve) => {
+    oauthService.configure(authCodeFlowConfig);
+    oauthService.setupAutomaticSilentRefresh();
+    oauthService.loadDiscoveryDocumentAndLogin()
+      .then(() => resolve());
+  });
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideOAuthClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (oauthService: OAuthService) => {
+        return () => {
+          initializeOAuth(oauthService);
+        }
+      },
+      multi: true,
+      deps: [
+        OAuthService
+      ]
+    }
+  ]
+};
