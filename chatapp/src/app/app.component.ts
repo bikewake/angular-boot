19e3fc8ea6c919ba2b4ed4086770d4002eecdd14
Lubo Sladok <bikewake@gmail.com>
2023-11-22 18:36:25 +0100

    add authorized content

--- chatapp/src/app/app.component.ts
+++ chatapp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'chatapp';
+  helloText = '';
+
+  constructor(private oauthService: OAuthService, private httpClient: HttpClient) { }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  getHelloText() {
+    this.httpClient.get<{ message: string }>('http://localhost:8080/hello', {
+      headers: {
+        'Authorization': `Bearer ${this.oauthService.getAccessToken()}`
+      }
+    }).subscribe(result => {
+      this.helloText = result.message;
+    });
+  }
 }
