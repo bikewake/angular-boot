81e72a4d34fecc729977d0435d37fd0c65df487c
Lubo Sladok <bikewake@gmail.com>
2023-11-28 09:12:16 +0100

    add keycloak to stream-chat

--- src/main/java/org/bikewake/stream/config/SecurityConfig.java
+++ src/main/java/org/bikewake/stream/config/SecurityConfig.java
@@ -21,6 +21,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/*.js*").permitAll()
+                        .requestMatchers("/*.css*").permitAll()
+                        .requestMatchers("/index.html*").permitAll()
+                        .requestMatchers("/?continue").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(
--- stream-chat/src/app/app.component.ts
+++ stream-chat/src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'streamchat';
+export class AppComponent implements OnInit {
+  title = 'Stream Chat';
+
+  public isLoggedIn = false;
+  public userProfile: KeycloakProfile | null = null;
+
+  constructor(private readonly keycloak: KeycloakService) {}
+
+  public async ngOnInit() {
+    this.isLoggedIn = await this.keycloak.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      this.userProfile = await this.keycloak.loadUserProfile();
+    }
+  }
+
+  public login() {
+    this.keycloak.login();
+  }
+
+  public logout() {
+    this.keycloak.logout();
+  }
 }
