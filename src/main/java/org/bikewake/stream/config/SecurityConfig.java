094b6a996d541bd2ddb77bfb3ad33dbbeaa288f3
Lubo Sladok <bikewake@gmail.com>
2023-11-28 12:53:27 +0100

    add /hello path to spring security config

--- src/main/java/org/bikewake/stream/config/SecurityConfig.java
+++ src/main/java/org/bikewake/stream/config/SecurityConfig.java
@@ -21,12 +21,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/*.js*").permitAll()
-                        .requestMatchers("/*.css*").permitAll()
-                        .requestMatchers("/index.html*").permitAll()
-                        .requestMatchers("/?continue").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/hello").authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(
                         jwt -> jwt.jwtAuthenticationConverter(keycloakJwtConverter())
