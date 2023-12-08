57768799cffa4c9b4f4638510629d858166b4b22
Lubo Sladok <bikewake@gmail.com>
2023-12-08 09:22:30 +0100

    add angular components and services

---
+++ speak-stream/src/app/hike/hike.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hike',
+  standalone: true,
+  imports: [],
+  templateUrl: './hike.component.html',
+  styleUrl: './hike.component.css'
+})
+export class HikeComponent {
+
+}
---
+++ speak-stream/src/app/wake/wake.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wake',
+  standalone: true,
+  imports: [],
+  templateUrl: './wake.component.html',
+  styleUrl: './wake.component.css'
+})
+export class WakeComponent {
+
+}
