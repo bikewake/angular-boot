a29fc34d1992330c2dfb7136ea46a6589bc62d57
Lubo Sladok <bikewake@gmail.com>
2023-12-08 16:43:00 +0100

    add keycloak service

--- speak-stream/src/app/app.component.ts
+++ speak-stream/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet,  RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,25 +6,39 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,  RouterLink, RouterLinkActive,
-            MatIconModule, MatDividerModule, MatMenuModule, MatToolbarModule, MatButtonModule],
+            MatIconModule, MatDividerModule, MatMenuModule, MatToolbarModule, MatButtonModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'speak-stream';
 
   isLoggedIn = false;
+  userProfile: KeycloakProfile | null = null;
 
-    public login() {
-//      this.keycloak.login();
-    }
+  constructor(private readonly keycloak: KeycloakService) {}
 
-    public logout() {
-//      this.keycloak.logout();
+  public async ngOnInit() {
+    this.isLoggedIn = await this.keycloak.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      this.userProfile = await this.keycloak.loadUserProfile();
     }
+  }
+
+  public login() {
+    this.keycloak.login();
+  }
+
+  public logout() {
+    this.keycloak.logout();
+  }
 }
--- speak-stream/src/app/app.config.ts
+++ speak-stream/src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig, isDevMode, ErrorHandler } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, isDevMode, ErrorHandler } from '@angular/core';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from "@angular/common/http";
 import { provideRouter } from '@angular/router';
 import { ErrorLogHandlerService } from './error-log-handler.service';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { KeycloakAngularModule, KeycloakService, KeycloakBearerInterceptor } from 'keycloak-angular';
+import { keyCloakConfig } from './environment';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: keyCloakConfig,
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +27,25 @@ export const appConfig: ApplicationConfig = {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     }),
+    provideHttpClient(
+        withInterceptorsFromDi()
+    ),
     provideAnimations(),
+    KeycloakAngularModule,
+    KeycloakService,
+    KeycloakBearerInterceptor,
+    {
+                provide: HTTP_INTERCEPTORS,
+                useClass: KeycloakBearerInterceptor,
+                multi: true,
+                deps: [KeycloakService]
+    },
+    {
+          provide: APP_INITIALIZER,
+          useFactory: initializeKeycloak,
+          multi: true,
+          deps: [KeycloakService]
+    },
     { provide: ErrorHandler, useClass: ErrorLogHandlerService }
   ]
 };
